173d0eb2d400a40313214cc49edea449d6cf5ac0
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:11:32 +0530

    Update tournament.h

--- tournament.h
+++ tournament.h
@@ -1,13 +1,11 @@
 #ifndef TOURNAMENT_H
 #define TOURNAMENT_H
 
-#include "player.h"
-#include <vector>
-#include <string>
-using namespace std;
+#include "player.h" // Includes vector, string
+using namespace std; 
 
 void displayBracket(const vector<Player*>& players, const string& roundName);
 vector<Player*> runTournamentRound(vector<Player*>& contestants, int teamSize, const string& roundName);
-void startTournament(vector<Player>& allPlayersInGame); // Takes a list of players who will register
+void startTournament(vector<Player>& allPlayersInGame);
 
 #endif // TOURNAMENT_H
