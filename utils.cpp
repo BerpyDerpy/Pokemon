a0b198fbaa9bfa72aafe68a11e311ed08644becf
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:13:01 +0530

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,7 @@
 #include "utils.h"
-#include <iostream>
 #include <cstdlib> // For system() - basic clear screen
-using namespace std;
+
+using namespace std; 
 
 // Static random number generator setup for better distribution
 random_device rd_utils;
@@ -18,22 +18,22 @@ void clearScreen() {
 
 int getRandomInt(int min, int max) {
     if (min > max) {
-        swap(min, max);
+        swap(min, max); 
     }
     uniform_int_distribution<> distrib(min, max);
     return distrib(gen_utils);
 }
 
-int getIntInput(const string& prompt, int minRange, int maxRange) {
+int getIntInput(string& prompt, int minRange, int maxRange) {
     int choice;
     while (true) {
-        cout << prompt;
-        cin >> choice;
+        cout << prompt; 
+        cin >> choice;  
         if (cin.good() && choice >= minRange && choice <= maxRange) {
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
             return choice;
         } else {
-            cout << "Invalid input. Please enter a number between " << minRange << " and " << maxRange << "." << endl;
+            cout << "Invalid input. Please enter a number between " << minRange << " and " << maxRange << "." << endl; 
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
