cf0f3e7651999cea34ba8f27ccb6354f6c1d7b7f
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:01:57 +0530

    Update battle_logic.h

--- battle_logic.h
+++ battle_logic.h
@@ -1,24 +1,22 @@
 #ifndef BATTLE_LOGIC_H
 #define BATTLE_LOGIC_H
 
-#include "player.h" // Battle involves players
-#include "move.h"
-#include "pokemon.h"
-#include <map>
-
+#include "player.h" // Battle involves players (also brings in pokemon.h, move.h)
+#include <map>     
+using namespace std;
 // Calculate type effectiveness multiplier
-float getTypeEffectiveness(const std::string& moveType, const std::string& defenderType1, const std::string& defenderType2);
+float getTypeEffectiveness( string& moveType, string& defenderType1, string& defenderType2);
 
 // Calculate damage
-int calculateDamage(const Move& move, const Pokemon& attacker, const Pokemon& defender);
+int calculateDamage( Move& move, Pokemon& attacker, Pokemon& defender);
 
 // Attempt dodge
-bool attemptDodge(const Pokemon& defender, int dodgeChancePercent = 10); // e.g., 10% base dodge chance
+bool attemptDodge( Pokemon& defender, int dodgeChancePercent = 10);
 
 // Core battle turn execution
 void executeTurn(Player& attackerPlayer, Player& defenderPlayer, int moveChoice);
 
 // Main battle loop for two players
-Player* startBattle(Player& player1, Player& player2, bool isGymBattle = false); // Returns pointer to the winning player or nullptr for a draw
+Player* startBattle(Player& player1, Player& player2, bool isGymBattle = false);
 
 #endif // BATTLE_LOGIC_H
