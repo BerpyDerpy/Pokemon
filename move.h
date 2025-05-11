aa6e0c24f4eae6f077be285b127ee88e3729034d
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:06:20 +0530

    Update move.h

--- move.h
+++ move.h
@@ -2,16 +2,19 @@
 #define MOVE_H
 
 #include <string>
+#include <iostream>
+
+using namespace std; 
 
 struct Move {
-    std::string name;
-    std::string type;
+    string name;
+    string type;
     int minDamage;
     int maxDamage;
     int currentPP;
     int maxPP;
 
-    Move(std::string n = "Struggle", std::string t = "Normal", int minD = 5, int maxD = 10, int pp = 1)
+    Move(string n = "Struggle", string t = "Normal", int minD = 5, int maxD = 10, int pp = 1)
         : name(n), type(t), minDamage(minD), maxDamage(maxD), currentPP(pp), maxPP(pp) {}
 
     void displayMove() const;
