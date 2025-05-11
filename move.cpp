1e0ba2a3534946b1abe582046495088c2d4907c0
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:05:47 +0530

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -1,6 +1,4 @@
 #include "move.h"
-#include <iostream>
-using namespace std;
 
 void Move::displayMove() const {
     cout << name << " (" << type << ") - DMG: " << minDamage << "-" << maxDamage << " PP: " << currentPP << "/" << maxPP;
