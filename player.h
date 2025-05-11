9c4327a71571d717b4717f646eb639b15e88868a
Mahit Hazari <mahithazari@gmail.com>
2025-05-11 09:08:08 +0530

    Update player.h

--- player.h
+++ player.h
@@ -4,24 +4,22 @@
 #include <string>
 #include <vector>
 #include "pokemon.h" // Player has Pokemon
-using namespace std;
 
+using namespace std; 
 struct Player {
     string name;
-    vector<Pokemon> pokemonParty;      // All Pokemon the player owns (up to 6 for a standard battle)
-    vector<int> activePokemonIndices; // Indices of Pokemon chosen for the current battle/tournament round
-                                           // relative to pokemonParty. This makes switching easier.
-    Pokemon* currentPokemon;               // Pointer to the currently active Pokemon in battle from pokemonParty
+    vector<Pokemon> pokemonParty;      // All Pokemon the player owns
+    vector<int> activePokemonIndices; // Indices of Pokemon chosen for the current battle
+    Pokemon* currentPokemon;          // Pointer to the currently active Pokemon in battle
     int money;
-    // vector<string> items; // For future item implementation
 
     Player(string n = "Trainer") : name(n), currentPokemon(nullptr), money(1000) {}
 
     void displayPartySummary() const;
     bool canBattle() const; // Checks if any Pokemon in the active party can battle
     void healParty();       // Heals all Pokemon in pokemonParty
-    void chooseActivePokemonForBattle(int teamSize); // Allows player to select 'teamSize' Pokemon for activePokemonIndices
-    Pokemon* getFirstAvailablePokemon(); // Gets the next available Pokemon from active list
+    void chooseActivePokemonForBattle(int teamSize);
+    Pokemon* getFirstAvailablePokemon();
 };
 
 #endif // PLAYER_H
